9acfaa879b37540bc35bf24bdd83afaf178cd204
Paulo D'Almeida <paulo.dalmeida@trsolucoes.com>
2023-05-23 09:19:37 -0300

    initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,15 @@
+function toggleModel() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  const img = document.querySelector('#profile img')
+
+  if(html.classList.contains('light')) {
+
+    img.setAttribute('src', './assets/avatar2.png')
+
+  } else {
+
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
